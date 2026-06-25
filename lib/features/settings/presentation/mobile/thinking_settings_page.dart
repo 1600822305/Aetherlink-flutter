6e5eff16100c6226787cf2ec633d4a23454cae36
Joshua Johnson <suNNBzjDHn@outlook.com>
2026-06-25 04:16:08 +0000

    Revert "Merge pull request #366 from 1600822305/devin/1782357913-timeline-polish"
    This reverts commit 21521f10b3218875d9d7739ce0c2458fa9b2893d, reversing
    changes made to f87be8cee8b801b5577cb9bbd064ce5e495f1697.

--- lib/features/settings/presentation/mobile/thinking_settings_page.dart
+++ lib/features/settings/presentation/mobile/thinking_settings_page.dart
@@ -272,7 +272,6 @@ class _PreviewCardState extends State<_PreviewCard> {
                     thinkingSeconds: 3.5,
                     isThinking: false,
                     thoughtAutoCollapse: settings.thoughtAutoCollapse,
-                    showThinkingContent: true,
                     inlineToolBlocks: _sampleToolBlocks,
                     markdownBuilder: (context, content, style) => GptMarkdown(
                       content,
